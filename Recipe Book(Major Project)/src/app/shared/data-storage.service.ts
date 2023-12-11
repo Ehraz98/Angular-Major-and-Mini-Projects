0cee7bd49a8332222c9f7de7163b7f08dd4ef969
Ehraz98 <ehrazahmed14@gmail.com>
2023-12-11 23:55:56 +0000

    Adding Http Requests via Firebase

---
+++ Recipe Book(Major Project)/src/app/shared/data-storage.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+
+  constructor(private httpClient: HttpClient,
+    private recipeService: RecipeService) {}
+
+  storeRecipes()
+  {
+    const recipes = this.recipeService.getRecipes();
+    this.httpClient.put('https://recipe-book-597df-default-rtdb.firebaseio.com/recipes.json',recipes).subscribe(
+      (res)=>{
+        console.log(res);
+      }
+    );
+  }
+
+  fetchRecipes()
+  {
+    return this.httpClient.get<Recipe[]>('https://recipe-book-597df-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(map(recipes=>{
+      return recipes.map(recipe=> {
+        return {... recipe, ingredients: recipe.ingredients? recipe.ingredients:[]};
+      })
+    }),
+    tap(recipes=>{
+      this.recipeService.setRecipes(recipes);
+    })
+    );
+  }
+}
