0f988b2826388b67501dbaaffdcd0ab1cd359d6d
Ehraz98 <ehrazahmed14@gmail.com>
2023-12-13 01:23:31 +0000

    Adding Authrentication in Recipe BOOk

--- Recipe Book(Major Project)/src/app/shared/data-storage.service.ts
+++ Recipe Book(Major Project)/src/app/shared/data-storage.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { map, tap } from 'rxjs/operators';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,29 +12,29 @@ import { map, tap } from 'rxjs/operators';
 export class DataStorageService {
 
   constructor(private httpClient: HttpClient,
-    private recipeService: RecipeService) {}
+    private recipeService: RecipeService,
+    private authService: AuthService) { }
 
-  storeRecipes()
-  {
+  storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    this.httpClient.put('https://recipe-book-597df-default-rtdb.firebaseio.com/recipes.json',recipes).subscribe(
-      (res)=>{
+    this.httpClient.put('https://recipe-book-597df-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(
+      (res) => {
         console.log(res);
       }
     );
   }
 
-  fetchRecipes()
-  {
-    return this.httpClient.get<Recipe[]>('https://recipe-book-597df-default-rtdb.firebaseio.com/recipes.json')
-    .pipe(map(recipes=>{
-      return recipes.map(recipe=> {
-        return {... recipe, ingredients: recipe.ingredients? recipe.ingredients:[]};
+  fetchRecipes() {
+      return this.httpClient.get<Recipe[]>('https://recipe-book-597df-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+        })
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
       })
-    }),
-    tap(recipes=>{
-      this.recipeService.setRecipes(recipes);
-    })
     );
   }
 }
