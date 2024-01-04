4be1dedb7cf3080a7a58b579f2f2307428a046c1
Ehraz98 <ehrazahmed14@gmail.com>
2024-01-04 20:11:54 +0000

    ngrx-basic-project-done

--- ngrx-basic-project/src/app/store/counter.actions.ts
+++ ngrx-basic-project/src/app/store/counter.actions.ts
@@ -9,3 +9,12 @@ export const decrement = createAction(
     '[Counter] Decrement',
     props<{value: number}>()
 );
+
+export const init = createAction(
+    '[Counter] Init'
+);
+
+export const set = createAction(
+    '[Counter] Set',
+    props<{value: number}>()
+);
